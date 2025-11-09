a0f92ad35e585393a40a045fd1e7613a6f57503c
blumea7 <mau.paunil@gmail.com>
2025-11-09 12:21:01 +0800

    Create solution for Puzzle 22

--- Solutions/Puzzle22-Occurences.sql
+++ Solutions/Puzzle22-Occurences.sql
@@ -34,15 +34,15 @@ Alpha		|	Status Complete						|	8
 
 */
 
-DROP TABLE IF EXISTS dbo.Occurrences
+DROP TABLE IF EXISTS dbo.Occurrences;
 
-CREATE TABLE dbo.Occurences (
+CREATE TABLE dbo.Occurrences (
 	Workflow VARCHAR(10)
 	, [Message] VARCHAR(50)
 	, Occurences TINYINT
 )
 
-INSERT INTO dbo.Occurences VALUES
+INSERT INTO dbo.Occurrences VALUES
 	('Bravo', 'Error: Cannot Divide by 0', '3')
 	, ('Alpha', 'Error: Conversion Failed', '5')
 	, ('Charlie', 'Error: Conversion Failed', '7')
@@ -51,3 +51,25 @@ INSERT INTO dbo.Occurences VALUES
 	, ('Charlie', 'Error: Unidentified error occurred', '10')
 	, ('Alpha', 'Status Complete', '8')
 	, ('Charlie', 'Status Complete', '6')
+
+-- Check Data
+
+SELECT * FROM dbo.Occurrences;
+
+-- Solution 
+
+WITH RankCTE AS (
+	SELECT
+		Workflow
+		, [Message]
+		, Occurences
+		, RANK() OVER(PARTITION BY Message ORDER BY Occurences DESC) AS RankOccurrences
+	FROM dbo.Occurrences
+)
+
+SELECT
+	Workflow
+	, [Message]
+	, Occurences
+FROM RankCTE
+	WHERE RankOccurrences = 1
